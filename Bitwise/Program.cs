9ee4e5be9f9e5e3f003b6f6e84902401b287bb73
Alexandre Previatti <previatti@planetinternet.com.br>
2022-08-23 14:47:18 -0400

    Add project files.

---
+++ Bitwise/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool sair = false;
+
+try
+{
+    while (!sair)
+    {
+        int a = 0; int b = 0; int c = 0;
+        Console.WriteLine("Informe o primeiro bitwise:");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o segundo bitwise:");
+        b = int.Parse(Console.ReadLine());
+
+        c = a & b;
+        if(c == 0)
+            Console.WriteLine($"{b} não contem {a}: {a & b}");
+        else
+            Console.WriteLine($"{b} contem {a}: {a & b}");
+
+        System.Console.WriteLine("acabou ------- digite 0 (zero) para sair...");
+        if (int.Parse(Console.ReadLine()) == 0) sair = true;
+    }
+}
+catch (Exception)
+{
+
+}
+
+
+
